7fdd75b21452bae4dc1c0098a15a084752af6101
Nave7374 <nave7374@gmail.com>
2025-06-08 15:50:29 +0530

    usual commit

--- src/components/FullPageLoader.jsx
+++ src/components/FullPageLoader.jsx
@@ -1,4 +1,4 @@
-import { Box, CircularProgress, rgbToHex } from "@mui/material";
+import { Box, CircularProgress } from "@mui/material";
 
 function FullpageLoader(){
 
--- src/pages/Shipment/ShipmentHistory.jsx
+++ src/pages/Shipment/ShipmentHistory.jsx
@@ -40,14 +40,14 @@ function ShipmentHistory(){
                 </TableHead>
                 <TableBody>
                     {
-                        shipmenthistory.map((history,index)=>{
+                        shipmenthistory.map((history,index)=>{ return(
                             <TableRow key={index}>
                                 <TableCell>{history.shipmentnumber}</TableCell>
                                 <TableCell>{history.origin}</TableCell>
                                 <TableCell>{history.destination}</TableCell>
                                 <TableCell>{new Date(history.date).toLocaleDateString()}</TableCell>
                                 <TableCell>{history.user.firstName+" "+history.user.lastName}</TableCell>
-                            </TableRow>
+                            </TableRow>)
                         })
                     }
                 </TableBody>
